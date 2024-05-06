250f417a4c904e58d9700cb1ce0b6768d5620d80
himatoko <160095919+himatoko@users.noreply.github.com>
2024-05-06 03:19:18 +0000

    [Update] tableの整理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = Book.where(user_id: @user.id)
   end
   
   def index
