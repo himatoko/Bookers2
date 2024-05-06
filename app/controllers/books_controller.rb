35a1061561bb471e719a4948810fe13366887af5
himatoko <160095919+himatoko@users.noreply.github.com>
2024-05-06 16:09:01 +0000

    [Add] Bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,8 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
